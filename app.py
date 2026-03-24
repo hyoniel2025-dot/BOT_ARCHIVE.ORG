15508bbd197f8aee54577258b2009ee9f25e5647
hyoniel2025-dot <hyoniel2025@gmail.com>
2026-03-24 13:50:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot activo ✅"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
+
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 import uuid, os
